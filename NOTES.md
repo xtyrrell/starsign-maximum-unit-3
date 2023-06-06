1f46dc4670ab04d0ebd0f90c5fd69ad53b1bf081
Max Tyrrell <m@xtyrrell.com>
2023-06-06 20:58:42 +0200

    feat: generate PDFs better

--- NOTES.md
+++ NOTES.md
@@ -1,14 +1,37 @@
 # Todos
 
-- [ ] add location picking
-- [ ] make feeling show image and text
-- [ ] choose seperator of username+password
+- [x] add location picking
+- [x] make feeling show image and text
+  - [x] just need to add a data URL of the feeling image to the feeling JSON item under imageUrl
+- [x] make end screen loading screen (simple rotating loading spinner)
 
-- [ ] make available offline
-  - [ ] download libraries on frontend and change script tags
+- [x] instructions on each page
+
+- [x] add star signs
+  - [x] make star sign run along top/bottom of page
+- [x] add animals
+- [x] add confession
+- [x] display sporty + bohemian + cyber percentages at the end
+- [x] add aura drawing
+
+- [ ] LINK all pages TOGETHER!
+- [x] add new animals
+
+- [x] probably should make animals all same size (https://stackoverflow.com/a/56273522)
+
+- [ ] fix newline in between username+password
+
 - [ ] add clothing from emails
-- [ ] add star signs
-- [ ] add aura drawing
-- [ ] add animals
-- [ ] add confession page
-- [ ] display sporty + bohemian + cyber percentages at the end
+- [ ] make aura x 0.65
+
+- [x] make available offline
+  - [x] download libraries on frontend and change script tags
+
+- [x] fix size of feeling image in generated PDF
+
+
+- [ ] DOWNLOAD JSON FILE!!
+
+## linking
+
+login -> avatar -> feeling -> map -> animal -> confession -> star-sign -> aura -> end
--- layout-and-printing/layout/generate_pdf.py
+++ layout-and-printing/layout/generate_pdf.py
@@ -2,8 +2,11 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
 from reportlab.lib.utils import ImageReader
+from reportlab.platypus import Paragraph
 
 from sys import argv
+
+from random import randint
 
 import json
 
@@ -17,36 +20,86 @@ TEXT_WIDTH = 40 * mm
 TEXT_HEIGHT = 40 * mm
 
 
-def draw_image_element(data_url, c):
+def draw_image_element(c, data_url):
     image = ImageReader(data_url)
+    imgwidth, imgheight = image.getSize()
 
-    x, y = generate_random_image_position(IMAGE_WIDTH, IMAGE_HEIGHT)
+    x, y = generate_random_image_position(imgwidth, imgheight)
 
-    c.drawImage(image=image, x=x, y=y, width=IMAGE_WIDTH,
-                height=IMAGE_HEIGHT, preserveAspectRatio=True,
+    c.drawImage(image=image, x=x, y=y, width=imgwidth,
+                height=imgheight, preserveAspectRatio=True,
                 mask='auto')
 
 
-def draw_text_element(text, c):
+def draw_text_element(c, text, fontname='Helvetica', fontsize=16):
     x, y = generate_random_image_position(TEXT_WIDTH * 3, TEXT_HEIGHT)
-
+    
+    c.setFont(fontname, fontsize)
     c.drawString(x, y, text)
+
+def draw_text_element_as_border(c, text, fontname='Helvetica', fontsize=16):
+    # c = canvas.Canvas('aa', A4)
+    c.setFont(fontname, fontsize)
+
+    text = ("  " + text + "  ") * 100
+
+    # c = canvas.Canvas('P')
+   
+    # Top line
+    c.drawCentredString(A4[0] / 2, 10 * mm, text)
+    # Bottom line
+    c.drawCentredString(A4[0] / 2, A4[1] - (10 * mm), text)
+    # Right line
+    # c.drawCentredString(A4[0] / 2, A4[1] - (10 * mm), text)
+
+def draw_image_with_text(c, data_url, text, fontname='Helvetica', fontsize=16):
+    image = ImageReader(data_url)
+    imgwidth, imgheight = image.getSize()
+
+    x, y = generate_random_image_position(imgwidth, imgheight)
+
+    c.drawImage(image=image, x=x, y=y, width=imgwidth,
+                height=imgheight, preserveAspectRatio=True,
+                mask='auto')
+    c.setFont(fontname, fontsize)
+    c.drawString(x, y + imgheight + 5 * mm, text)
+
+
+def draw_scoring(c):
+    c.setFont("Times-Italic", 12)
+
+    cyber = randint(0, 100)
+    sporty = randint(0, cyber)
+    bohemian = 100 - cyber - sporty
+    scoring_text = f'Cyber: {cyber}%   Sporty: {sporty}%   Bohemian: {bohemian}%'
+    c.drawCentredString(A4[0] / 2, 25 * mm, scoring_text)
 
 
 def generate_pdf(items, output_filename):
     c = canvas.Canvas(output_filename, A4)
 
     for i, element in enumerate(items):
+        if element.get('label') == 'star-sign':
+            draw_text_element_as_border(c, element['text'], 'Times-Italic', 14)
+            continue
+        if element.get('label') == 'aura':
+            draw_image_with_text(c, element['imageUrl'], '** My aura looks like: **', 'Times-Italic', 14)
+            continue
+
         if element.get('imageUrl'):
-            draw_image_element(element['imageUrl'], c)
+            draw_image_element(c, element['imageUrl'])
         if element.get('text'):
-            draw_text_element(element['text'], c)
+            draw_text_element(c, element['text'], 'Times-Italic' if element['label'] == 'star-sign' else 'Helvetica', 16)
+        
+    draw_scoring(c)
 
     c.save()
 
 if __name__ == '__main__':
     items_filename = argv[1]
     output_filename = argv[2]
+
+    print("Generating PDF from {} to {}".format(items_filename, output_filename))
 
     with open(items_filename) as f:
         items = json.load(f)
