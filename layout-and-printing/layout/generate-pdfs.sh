d317346743d90840caa0bf94b7015d92b6575b67
Max Tyrrell <m@xtyrrell.com>
2023-06-06 21:33:52 +0200

    fix: quote filename

--- layout-and-printing/layout/generate-pdfs.sh
+++ layout-and-printing/layout/generate-pdfs.sh
@@ -5,9 +5,9 @@ directory=watched-event-logs
 # because OpenKiosk is downloading files weirdly named...
 for f in $(find $directory -maxdepth 1 -type f -name "*.txt.part"); do
   echo "Found file $f"
-  python3 generate_pdf.py $f ../printing/watched-files-to-print/$(date +%s).pdf
+  python3 generate_pdf.py "$f" "../printing/watched-files-to-print/$(date +%s).pdf"
   sleep 5
-  mv $f ../../finished
+  mv "$f" ../../finished
 done
 
 # for f in $(find $directory -maxdepth 1 -type f -name "*.json"); do
